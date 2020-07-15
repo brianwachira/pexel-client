25c76293a42d706593f68c2525de18e0dcfda046
brianwachira <brianwachira7@gmail.com>
2020-07-15 12:14:12 +0300

    add document.ready function

--- src/main.js
+++ src/main.js
@@ -2,3 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+import {PexelService} from './js/pexels-service.js';
+
+
+$(document).ready(function(){
+
+  
+  (async () => {
+    let pexelService = new PexelService();
+    const photoResponse = await pexelService.getPictures("nature");
+
+    console.log(photoResponse.photos);
+  })();
+
+});

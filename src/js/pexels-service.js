caf93cf0bde36217986e2f17484b071edf92fa3b
brianwachira <brianwachira7@gmail.com>
2020-07-15 23:05:47 +0300

    edit async function to accept numofresults argument

--- src/js/pexels-service.js
+++ src/js/pexels-service.js
@@ -2,14 +2,14 @@ import { createClient } from 'pexels';
 
 export class PexelService{
 
-  async getPictures(topic){
+  async getPictures(topic,numOfResults){
 
     let jsonifiedResponse;
     const client = createClient(process.env.API_KEY);
   
     const query = topic;
   
-    await client.photos.search({ query, per_page: 16 }).then(photoResponse => {
+    await client.photos.search({ query, per_page: parseInt(numOfResults) }).then(photoResponse => {
       jsonifiedResponse = photoResponse;
     });
 

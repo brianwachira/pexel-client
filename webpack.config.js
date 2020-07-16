530efeb1561e41c90a9624393d52dc08601a892d
brianwachira <brianwachira7@gmail.com>
2020-07-16 15:41:57 +0300

    add rule for Dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,9 @@ module.exports = {
       template : './src/index.html',
       inject : 'body'
     }),
-    new Dotenv()
+    new Dotenv({
+      systemvars: true
+    })
   ],
   module : {
     rules : [

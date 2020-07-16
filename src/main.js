34f5e4ce3d8c015aaf7724c190a7d486453f27f7
brianwachira <brianwachira7@gmail.com>
2020-07-16 16:05:58 +0300

    modify styleupsearchbar and add clearimages function

--- src/main.js
+++ src/main.js
@@ -9,21 +9,34 @@ import {PexelService} from './js/pexels-service.js';
 $(document).ready(function(){
 
   function displayImages(photoObject){
-    $(".images").append(`<div class="col-md-3">
+    $(".images").append(`<div class="col-md-3 imagesDisplayed">
     <figure class='figure'>
       <img src=${ photoObject.src.portrait }  class="img-fluid rounded" id=${ photoObject.photographer_id }>
       <figcaption class="figure-caption text-center">Photographer : <a href=${ photoObject.photographer_url } target="_blank">${ photoObject.photographer }</a></figcaption> <a href=""></a>
     </figure></div>`);
   }
 
-  function styleUpSearchBar(){
+  function clearImages(){
+    $(".imagesDisplayed").remove();
+  }
+
+  function styleUpSearchBar(imagesAreComing){
+    if(imagesAreComing){
+
     $("#row").removeClass("vh-100");
     $("#row").addClass("pt-4");
+    }else{
+      $("#row").addClass("vh-100");
+      $("#row").removeClass("pt-4");
+    }
   }
 
   $("form#get-images").submit(function(){
     
     event.preventDefault();
+
+    clearImages();
+    styleUpSearchBar(false);
 
     const requestedImage = $("input#searchText").val();
     const numOfResults = $("input#numOfResults").val();
@@ -35,7 +48,7 @@ $(document).ready(function(){
 
         displayImages(photo);
       });
-      styleUpSearchBar();
+      styleUpSearchBar(true);
     })();
   });
 });

eec684f29db5c2cd3322acfcc3937c55bde96398
brianwachira <brianwachira7@gmail.com>
2020-08-13 12:54:37 +0300

    make column responsive on ln 12

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {PexelService} from './js/pexels-service.js';
 $(document).ready(function(){
 
   function displayImages(photoObject){
-    $(".images").append(`<div class="col-md-3 imagesDisplayed">
+    $(".images").append(`<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 imagesDisplayed">
     <figure class='figure'>
       <img src=${ photoObject.src.portrait }  class="img-fluid  img-thumbnail bg-dark rounded" id=${ photoObject.photographer_id } alt='search results'>
       <figcaption class="figure-caption text-center">Photographer : <a href=${ photoObject.photographer_url } target="_blank" class="text-dark">${ photoObject.photographer }</a></figcaption> <a href=""></a>
